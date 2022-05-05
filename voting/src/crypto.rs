3c264d1b08f751b94f4c22a15f1b2da95fbb0514
Mikkel Bengtsen <mikkelbengtsen@gmail.com>
2022-05-05 13:39:27 +0200

    cleanup and better document crypto.rs

--- voting/src/crypto.rs
+++ voting/src/crypto.rs
@@ -1,3 +1,7 @@
+//! Rust file containing the required on-chain crypto functions needed in the *voting* contract.
+//! 
+//! These are verifications of ZKPs, checking vote commitments and brute forcing the final tally.
+
 use crate::{OneInTwoZKP, SchnorrProof};
 use concordium_std::{trap, Vec};
 use group::GroupEncoding;
@@ -5,29 +9,32 @@ use k256::elliptic_curve::{PublicKey, ScalarCore, SecretKey};
 use k256::{ProjectivePoint, Scalar, Secp256k1};
 use sha2::{Digest, Sha256};
 
-// Check dl zkp: g^w = g^r * g^xz
-pub fn verify_dl_zkp(g_x: ProjectivePoint, schnorr: SchnorrProof) -> bool {
+/// Check Schnorr ZKP: g^w = g^r * g^xz
+pub fn verify_schnorr_zkp(g_x: ProjectivePoint, schnorr: SchnorrProof) -> bool {
     let g_w = convert_vec_to_point(schnorr.g_w);
     let r: Scalar = convert_vec_to_scalar(schnorr.r);
+
+    // Create hash z = H(g, g^w, g^x)
     let value_to_hash = ProjectivePoint::GENERATOR + g_w + g_x;
-    let z_hash_value = Sha256::digest(value_to_hash.to_bytes());
-    let z: Scalar = From::<&'_ ScalarCore<Secp256k1>>::from(&unwrap_abort(
-        ScalarCore::from_be_slice(&z_hash_value).ok(),
-    ));
+    let z = hash_to_scalar(value_to_hash.to_bytes().to_vec());
+
     let g_r = ProjectivePoint::GENERATOR * r;
     let g_x_z = g_x * z;
     let g_rg_x_z: ProjectivePoint = g_x_z + g_r;
+
     if g_rg_x_z == g_w {
         return true;
     }
     false
 }
 
-pub fn verify_one_out_of_two_zkp(zkp: OneInTwoZKP, g_y: ProjectivePoint) -> bool {
-    let r1: Scalar = convert_vec_to_scalar(zkp.r1);
-    let r2: Scalar = convert_vec_to_scalar(zkp.r2);
-    let d1: Scalar = convert_vec_to_scalar(zkp.d1);
-    let d2: Scalar = convert_vec_to_scalar(zkp.d2);
+/// Check one-in-two ZKP: check v = 1 or v = 0 without knowing which
+pub fn verify_one_in_two_zkp(zkp: OneInTwoZKP, g_y: ProjectivePoint) -> bool {
+    let r1 = convert_vec_to_scalar(zkp.r1);
+    let r2 = convert_vec_to_scalar(zkp.r2);
+    let d1 = convert_vec_to_scalar(zkp.d1);
+    let d2 = convert_vec_to_scalar(zkp.d2);
+
     let x = convert_vec_to_point(zkp.x);
     let y = convert_vec_to_point(zkp.y);
     let a1 = convert_vec_to_point(zkp.a1);
@@ -35,12 +42,9 @@ pub fn verify_one_out_of_two_zkp(zkp: OneInTwoZKP, g_y: ProjectivePoint) -> bool
     let a2 = convert_vec_to_point(zkp.a2);
     let b2 = convert_vec_to_point(zkp.b2);
 
-    //c = H(i,x,y,a1,b1,a2,b2)
+    // c = H(g^x, y, a1, b1, a2, b2)
     let value_to_hash = x.clone() + y.clone() + a1.clone() + b1.clone() + a2.clone() + b2.clone();
-    let hash = Sha256::digest(&value_to_hash.to_bytes());
-    let c: Scalar = From::<&'_ ScalarCore<Secp256k1>>::from(&unwrap_abort(
-        ScalarCore::from_be_slice(&hash).ok(),
-    ));
+    let c = hash_to_scalar(value_to_hash.to_bytes().to_vec());
 
     if c != d1.clone() + d2.clone() {
         return false;
@@ -60,14 +64,16 @@ pub fn verify_one_out_of_two_zkp(zkp: OneInTwoZKP, g_y: ProjectivePoint) -> bool
     true
 }
 
+/// Check commitment matches actual vote
 pub fn check_commitment(vote: ProjectivePoint, commitment: Vec<u8>) -> bool {
     Sha256::digest(&vote.to_bytes().to_vec()).to_vec() == commitment
 }
 
-/// yes votes are tallied on chain
+/// Brute force and tally yes votes on-chain
 pub fn brute_force_tally(votes: Vec<ProjectivePoint>) -> i32 {
     // Set first vote as initial tally
     let mut tally = unwrap_abort(votes.get(0)).clone();
+
     for i in 1..votes.len() {
         // Add all the rest of the votes (curve points) to tally, e.g \prod g^xy*g^v (calculated differently due to additive curve)
         tally = tally + unwrap_abort(votes.get(i));
@@ -78,13 +84,15 @@ pub fn brute_force_tally(votes: Vec<ProjectivePoint>) -> i32 {
     let pg = ProjectivePoint::GENERATOR;
 
     // Go through all votes and brute force number of yes votes
-    //for i in 0.. {
     while current_g != tally {
         yes_votes += 1;
         current_g += &pg;
     }
     yes_votes
 }
+
+/// Utility functions
+/// TODO: move into separate util crate?
 
 pub fn convert_vec_to_scalar(vec: Vec<u8>) -> Scalar {
     let scalar_option = SecretKey::<Secp256k1>::from_be_bytes(&vec).ok();
@@ -102,6 +110,15 @@ pub fn convert_vec_to_point(vec: Vec<u8>) -> ProjectivePoint {
     return PublicKey::to_projective(&point);
 }
 
+pub fn hash_to_scalar(bytes_to_hash: Vec<u8>) -> Scalar {
+    let hash_value = Sha256::digest(bytes_to_hash);
+
+    return From::<&'_ ScalarCore<Secp256k1>>::from(&unwrap_abort(
+        ScalarCore::from_be_slice(&hash_value).ok(),
+    );
+}
+
+/// Don't panic (like regular unwrap), which is not recommended in WASM
 #[inline]
 pub fn unwrap_abort<T>(o: Option<T>) -> T {
     match o {
