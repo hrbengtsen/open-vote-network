515c398db80c71cbcbfdf01f0a8c89f07830a045
babbeløg <90256662+babbelog@users.noreply.github.com>
2022-05-26 14:09:29 +0200

    improved bash script

--- voting/testing_election.sh
+++ voting/testing_election.sh
@@ -3,7 +3,7 @@ for i in {0..39}; do {
   echo "Process \"$i\" started";
   $cmd & pid=$!
   PID_LIST+=" $pid";
-  (echo y; echo "123456")  | concordium-client contract update 5067 --entrypoint register --sender voter${i+1} --parameter-binary parameters/register_msgs/register_msg$j.bin --energy 100000 --amount 1;
+  (echo y; echo "123456")  | concordium-client contract update 5068 --entrypoint $1 --sender voter${i+1} --parameter-binary parameters/$1_msgs/$1_msg$i.bin --energy 200000 --amount 1;
 } done
 
 
