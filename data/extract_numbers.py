8f325419788c7519e721a8ccb9db446c2b41ff59
babbeløg <90256662+babbelog@users.noreply.github.com>
2022-06-01 12:28:48 +0200

    add python script for extract on-chain values

---
+++ data/extract_numbers.py
@@ -0,0 +1,29 @@
+import csv
+
+file = open('voting/vote_output.txt', 'r')
+cost = []
+energy = []
+# Using for loop
+for line in file:
+    if "Transaction is finalized" in line:
+        line_edit = line[132:]
+        line_edit = line_edit.replace(' CCD (',',')
+        line_edit = line_edit.replace(' NRG).\n', '')
+        i = line_edit.index(",")
+        cost.append(line_edit[0:i])
+        energy.append(line_edit[i+1:])
+
+with open('cost_csv_file.csv', 'w') as f:
+    # create the csv writer
+    writer = csv.writer(f)
+
+    # write a row to the csv file
+    writer.writerow(cost)
+
+with open('energy_csv_file.csv', 'w') as f:
+     # create the csv writer
+    writer = csv.writer(f)
+
+    # write a row to the csv file
+    writer.writerow(energy)
+
